a61a0b6fa1cf3c82b8d1e1c96a75a9ab6c97f585
mp2172 <markpotter21@hotmail.com>
2021-06-20 11:51:46 +0100

    Added BananaService

---
+++ BlazorBattles/Client/Shared/EventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Shared
+{
+    public class EventArgs
+    {
+        public int amount { get; set; }
+        public string operand { get; set; }
+    }
+}
